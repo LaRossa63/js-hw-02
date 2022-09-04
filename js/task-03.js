7637369014bfe794432936c624937173b54ee964
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-04 19:18:43 +0400

    Задание 3

--- js/task-03.js
+++ js/task-03.js
@@ -1,1 +1,9 @@
-console.log(3);
+const findLongestWord = function (string) {
+  return string.split(' ').sort((a, b) => b.length - a.length)[0];
+};
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+
+console.log(findLongestWord('Google do a roll'));
+
+console.log(findLongestWord('May the force be with you'));
