d3175b6f7fecbb2e7234aeaa1bf9256772cbe7f0
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-04 19:05:47 +0400

    Задание 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,28 @@
-console.log(2);
+const calculateEngravingPrice = (message, pricePerWord) => {
+  const length = message.split(' ').length;
+  const totalPrice = pricePerWord * length;
+
+  return `За ${length} слов. К оплате: ${totalPrice}`;
+};
+
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    10
+  )
+);
+
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    20
+  )
+);
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40)
+);
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20)
+);
