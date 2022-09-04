40a1d8ed23290ec4b52232a82d02f39f05fa0fcd
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-04 19:54:41 +0400

    Задание 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,16 @@
-console.log(6);
+let input;
+const numbers = [];
+
+while (input !== null) {
+  input = prompt('Введите число');
+
+  if (input !== null) {
+    numbers.push(Number(input));
+  }
+}
+
+if (numbers.length !== 0) {
+  const total = numbers.reduce((total, element) => total + element, 0);
+
+  console.log(`Общая сумма чисел равна [${total}]`);
+}
