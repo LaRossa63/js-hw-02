bf8a68b31af6923cdfaea88d130a1edb028357db
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-04 19:21:29 +0400

    Задание 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,19 @@
-console.log(4);
+const formatString = (string) => {
+  if (string.length > 40) {
+    return `${string.slice(0, 40)}...`;
+  }
+
+  return string;
+};
+
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+
+console.log(formatString('Curabitur ligula sapien.'));
+
+console.log(
+  formatString(
+    'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.'
+  )
+);
