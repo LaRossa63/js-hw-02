e226ffa35085b888c7d0522e78ae071eb27a4732
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-04 19:47:20 +0400

    Задание 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,13 @@
-console.log(5);
+const checkForSpam = (message) => {
+  const text = message.toLocaleLowerCase();
+
+  return text.includes('spam') || text.includes('sale');
+};
+
+console.log(checkForSpam('Latest technology news'));
+
+console.log(checkForSpam('JavaScript weekly newsletter'));
+
+console.log(checkForSpam('Get best sale offers now!'));
+
+console.log(checkForSpam('[SPAM] How to earn fast money?'));
