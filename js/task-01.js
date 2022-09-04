5e08f2c86ac7974d1977322f50f0ba0a49a11791
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-04 19:00:03 +0400

    Задание 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,9 @@
-console.log(1);
+const logItems = (array) => {
+  array.forEach((element, index) =>
+    console.log(`[${element}] - [${index + 1}]`)
+  );
+};
+
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
